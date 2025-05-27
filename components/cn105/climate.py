760567c3a7a1495e96034df351f88564550b661e
ShaunIsOnFire <101719606+ShaunIsOnFire@users.noreply.github.com>
2025-05-27 10:17:00 -0400

    Update climate.py

--- components/cn105/climate.py
+++ components/cn105/climate.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components.uptime.sensor import UptimeSecondsSensor
+#from esphome.components.uptime.sensor import UptimeSecondsSensor
 
 from esphome.components import (
     climate,
